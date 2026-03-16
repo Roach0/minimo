e832d43d1adedb37b9d0c7b869cb758cb599185d
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2026-03-16 09:57:00 -0500

    drizzle adding
    we're in the process of adding drizzle this morning

---
+++ minimo/frontend/Header.tsx
@@ -0,0 +1,6 @@
+
+export default function Header() {
+    return (
+        <p>Welcome to Minimo</p>
+    )
+}
---
+++ minimo/frontend/MainPage.tsx
@@ -0,0 +1,26 @@
+import Header from "./Header"
+import Footer from "./Footer"
+import { useState } from "react"
+
+
+
+export default function MainPage() {
+    return(
+        <div>
+            <Header/>
+            <MainPageContent/>
+            <Footer/>
+        </div>
+    )
+}
+
+function MainPageContent () {
+    const [loggedIn , setLoggedIn] = useState(false)
+    let userName = "placeHolder"
+    return (
+        <div>
+            <h1>{!loggedIn ? "Welcome to Minimo Traveller!" : `Welcome back to Minimo ${userName}!`}</h1>
+            <button onClick={()=>{setLoggedIn(true)}}> Log In! </button>
+        </div>
+    )
+}
